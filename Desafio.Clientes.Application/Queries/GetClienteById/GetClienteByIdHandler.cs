10320dbaaa996e30cd28e73d0f20b7c59789f640
Raaffss <rafaellavagnini@gmail.com>
2025-09-01 13:42:42 -0300

    Adicionando GetClienteByIdQuery + handler

---
+++ Desafio.Clientes.Application/Queries/GetClienteById/GetClienteByIdHandler.cs
@@ -0,0 +1,35 @@
+﻿using Desafio.Clientes.Application.DTOs;
+using Desafio.Clientes.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.Clientes.Application.Queries.GetClienteById
+{
+    public class GetClienteByIdHandler : IRequestHandler<GetClienteByIdQuery, ClienteDto?>
+    {
+        private readonly IClienteRepository _repo;
+
+        public GetClienteByIdHandler(IClienteRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<ClienteDto?> Handle(GetClienteByIdQuery request, CancellationToken cancellationToken)
+        {
+            var cliente = await _repo.GetByIdAsync(request.Id);
+            if (cliente == null) return null;
+
+            return new ClienteDto
+            {
+                Id = cliente.Id,
+                NomeFantasia = cliente.NomeFantasia,
+                Cnpj = cliente.Cnpj.ToString(),
+                Ativo = cliente.Ativo
+            };
+        }
+    }
+}
---
+++ Desafio.Clientes.Application/Queries/GetClienteById/GetClienteByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using Desafio.Clientes.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.Clientes.Application.Queries.GetClienteById
+{
+    public record GetClienteByIdQuery(Guid Id) : IRequest<ClienteDto?>;
+}
